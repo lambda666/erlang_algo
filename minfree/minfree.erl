169c46f125e38cd77ac5df597897e5545b9c09ee
lambda666 <340035341@qq.com>
2021-03-12 15:51:16 +0800

    commit

--- minfree/minfree.erl
+++ minfree/minfree.erl
@@ -1,5 +1,5 @@
 -module(minfree).
--export([test/0, minfree/1]).
+-export([test/0, minfree/1, minfree_v2/1]).
 
 %% make_list(Len, InitValue)
 make_list(0, _, List) ->
@@ -51,5 +51,47 @@ minfree(List) ->
     io:format("~w ~n", [UsedList]),
     find_first(UsedList, 0).
 
+%% {List1, List2} = split(List)
+split([], _, _, List1, List2) ->
+    {List1, List2};
+split([Head|Rest], MinValue, MaxValue, List1, List2) ->
+    MiddleValue = (MaxValue - MinValue) div 2 + MinValue,
+    if
+        Head =< MiddleValue -> 
+            split(Rest, MinValue, MaxValue, [Head|List1], List2);
+        true ->
+            split(Rest, MinValue, MaxValue, List1, [Head|List2])
+    end.
+split(List, MinValue, MaxValue) ->
+    split(List, MinValue, MaxValue, [], []).
+
+%% minfree_v2(List)
+minfree_v2([], MinValue, _) ->
+    MinValue;
+minfree_v2(List, MinValue, MaxValue) ->
+    %io:format("~w~n", [List]),
+    MiddleValue = (MaxValue - MinValue) div 2 + MinValue,
+    %io:format("~w ~w ~w ~n", [MinValue, MiddleValue, MaxValue]),
+    {List1, List2} = split(List, MinValue, MaxValue),
+    %io:format("~w ~w ~n", [List1, List2]),
+    if
+        length(List1) == MiddleValue - MinValue + 1 ->
+            minfree_v2(List2, MiddleValue + 1, MaxValue);
+        true ->
+            minfree_v2(List1, MinValue , MiddleValue)
+    end.
+minfree_v2(List) ->
+    minfree_v2(List, 0, length(List)-1).
+
+%% make_arise_list(Len)
+make_arise_list(0,List) ->
+    List;
+make_arise_list(Len, List) ->
+    make_arise_list(Len-1, [Len|List]).
+make_arise_list(Len) ->
+    make_arise_list(Len, []).
+
 test() ->
-    minfree([0,1,2,3,4,6]).
+    List = make_arise_list(10000000),
+    minfree_v2(List).
+    %minfree_v2([0,2,3,4,6,5,5423,7,34]).
