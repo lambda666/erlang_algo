696a4252c7dca0a673179dab6f483db8199f9294
lambda <340035341@qq.com>
2021-03-15 21:09:32 +0800

    commit

--- minfree/minfree.erl
+++ minfree/minfree.erl
@@ -19,6 +19,19 @@ set_at([Head|Rest], Index, Value, NewList) ->
 set_at(List, Index, Value) ->
     set_at(List, Index, Value, []).
 
+%% set_at fold version
+set_at2(List, Index, Value) ->
+    F = fun(X, {Cnt, Acc}) ->
+        if
+            Cnt == Index ->
+                {Cnt+1, [Value|Acc]};
+            true ->
+                {Cnt+1, [X|Acc]}
+        end
+    end,
+    {_, Result} = lists:foldl(F, {0,[]}, List),
+    lists:reverse(Result).
+
 %% find_first(List, Value)
 find_first([], _, Index) ->
     Index;
@@ -29,11 +42,24 @@ find_first([_|Rest], Value, Index) ->
 find_first(List, Value) ->
     find_first(List, Value, 0).
 
+%% find_first fold version
+find_first2(List, Value) ->
+    F = fun(X, {Find, Acc}) ->
+        if 
+            X == Value , Find == -1 ->
+                {Acc, Acc+1};
+            true ->
+                {Find, Acc+1}
+        end
+    end,
+    {Find, _} = lists:foldl(F,{-1, 0}, List),
+    Find.
+
 %% check_used(UsedList, FlagList)
 check_used([], FlagList) ->
     FlagList;
 check_used([Head|Rest], FlagList) ->
-    check_used(Rest, set_at(FlagList, Head, 1)).
+    check_used(Rest, set_at2(FlagList, Head, 1)).
 
 %% minfree(List)
 minfree(List) ->
@@ -41,7 +67,7 @@ minfree(List) ->
     UsedList = check_used(List, FlagList),
     %io:format("~w ~n", [List]),
     %io:format("~w ~n", [UsedList]),
-    find_first(UsedList, 0).
+    find_first2(UsedList, 0).
 
 %% {List1, List2} = split(List)
 split([], _, _, List1, List2) ->
@@ -84,6 +110,6 @@ make_arise_list(Len) ->
     make_arise_list(Len, []).
 
 test() ->
-    List = make_arise_list(10000000),
+    List = make_arise_list(10000),
     minfree_v2(List).
     %minfree_v2([0,2,3,4,6,5,5423,7,34]).
