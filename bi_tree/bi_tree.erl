24c6967ceae079fcef966a669cf6933223c024c9
lambda <340035341@qq.com>
2021-03-22 22:04:20 +0800

    commit

--- bi_tree/bi_tree.erl
+++ bi_tree/bi_tree.erl
@@ -1,9 +1,32 @@
 -module(bi_tree).
 -compile(export_all).
 
--record(btree, {
-    lchild,
-    value,
-    rchild,
-    parent
+-record(bnode, {
+    lchild=null,
+    key=null,
+    rchild=null
 }).
+
+make_bnode(L,K,R) ->
+    #bnode{lchild=L,key=K,rchild=R}.
+
+insert(null, Key) ->
+    make_bnode(null, Key, null);
+insert(#bnode{lchild=L,key=K,rchild=R}, Key) ->
+    if
+        K == null ->
+            make_bnode(L, Key, R);
+        Key < K ->
+            make_bnode(insert(L, Key), K, R);
+        Key > K ->
+            make_bnode(L, K, insert(R, Key));
+        true ->
+            make_bnode(L, K, R)
+    end.
+
+
+test() ->
+    Node = make_bnode(null, null, null),
+    N1 = insert(Node, 1),
+    N2 = insert(N1, 2),
+    N2.
