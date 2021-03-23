fa65744e4060710ecf0b39e5cbb27473512519ab
lambda666 <340035341@qq.com>
2021-03-23 09:06:44 +0800

    commit

--- bi_tree/bi_tree.erl
+++ bi_tree/bi_tree.erl
@@ -24,13 +24,34 @@ insert(#bnode{lchild=L,key=K,rchild=R}, Key) ->
             make_bnode(L, K, R)
     end.
 
-preoder_trav(#bnode{lchild=L,key=K,rchild=R}) ->
-    preoder_trav(L),K,preoder_trav(R).
+concat([H|T], L2, Res) ->
+    concat(T, L2, [H|Res]);
+concat([], [H|T], Res) ->
+    concat([], T, [H|Res]);
+concat([], [], Res) ->
+    lists:reverse(Res).
+concat(L1, L2) ->
+    concat(L1, L2, []).
 
+preorder_trav(null) ->
+    [];
+preorder_trav(#bnode{lchild=L,key=K,rchild=R}) ->
+    concat([K|preorder_trav(L)],preorder_trav(R)).
 
+inorder_trav(null) ->
+    [];
+inorder_trav(#bnode{lchild=L,key=K,rchild=R}) ->
+    concat(inorder_trav(L),[K|inorder_trav(R)]).
+
+postorder_trav(null) ->
+    [];
+postorder_trav(#bnode{lchild=L,key=K,rchild=R}) ->
+    concat(concat(postorder_trav(L), postorder_trav(R)), [K]).
 
 test() ->
-    Node = make_bnode(null, null, null),
-    N1 = insert(Node, 1),
+    EmptyTree = make_bnode(null, null, null),
+    N1 = insert(EmptyTree, 5),
     N2 = insert(N1, 2),
-    N2.
+    N3 = insert(N2, 1),
+    N4 = insert(N3, 9),
+    inorder_trav(N4).
