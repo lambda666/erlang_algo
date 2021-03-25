77d09069486c60fa7cde14ae7bd14506479a9477
lambda666 <340035341@qq.com>
2021-03-25 10:03:22 +0800

    commit

--- rpn_calculator/rpn.erl
+++ rpn_calculator/rpn.erl
@@ -8,9 +8,9 @@
 
 -record(syntax_node, {
     l_child=null,
+    value='',
     r_child=null,
-    type=null,% operand or operator
-    value=''
+    type=null % operand or operator
 }).
 
 -record(syntax_tree_parser,{
@@ -20,42 +20,67 @@
     work_stack=[]
 }).
 
-syntax_parse(_) ->
-    null;
-syntax_parse(#syntax_tree_parser{l_tree=L, brackets_recursive=0, work_stack=[]}) ->
+syntax_parse0(#syntax_tree_parser{l_tree=L, brackets_recursive=0, work_stack=[]}) ->
+    io:format("~w ~n", [L]),
     L;
 
-syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, brackets_recursive=B, work_stack=['('|T]}) ->
-    syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, brackets_recursive=B+1, work_stack=T});
-syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, brackets_recursive=B, work_stack=[')'|T]}) ->
-    syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, brackets_recursive=B-1, work_stack=T});
-
-syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, brackets_recursive=B, work_stack=['+'|T]}) ->
+syntax_parse0(#syntax_tree_parser{l_tree=L, r_tree=R, brackets_recursive=B, work_stack=['+'|T]}) ->
+    io:format("L:~w ~n", [L]),
+    io:format("R:~w ~n", [R]),
     Node = #syntax_node{l_child=L, r_child=R, type=operator, value='+'},
-    syntax_parse(#syntax_tree_parser{l_tree=Node, r_tree=null, brackets_recursive=B, work_stack=T});
-syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, brackets_recursive=B, work_stack=['-'|T]}) ->
+    io:format("Node:~w ~n", [Node]),
+    syntax_parse0(#syntax_tree_parser{l_tree=Node, r_tree=null, brackets_recursive=B, work_stack=T});
+syntax_parse0(#syntax_tree_parser{l_tree=L, r_tree=R, brackets_recursive=B, work_stack=['-'|T]}) ->
     Node = #syntax_node{l_child=L, r_child=R, type=operator, value='-'},
-    syntax_parse(#syntax_tree_parser{l_tree=Node, r_tree=null, brackets_recursive=B, work_stack=T});
-syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, brackets_recursive=B, work_stack=['*'|T]}) ->
-    Node = #syntax_node{l_child=L, r_child=R, type=operator, value='*'},
-    syntax_parse(#syntax_tree_parser{l_tree=Node, r_tree=null, brackets_recursive=B, work_stack=T});
-syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, brackets_recursive=B, work_stack=['/'|T]}) ->
-    Node = #syntax_node{l_child=L, r_child=R, type=operator, value='/'},
-    syntax_parse(#syntax_tree_parser{l_tree=Node, r_tree=null, brackets_recursive=B, work_stack=T});
+    syntax_parse0(#syntax_tree_parser{l_tree=Node, r_tree=null, brackets_recursive=B, work_stack=T});
 
-syntax_parse(#syntax_tree_parser{l_tree=null, r_tree=null, brackets_recursive=B, work_stack=[H|T]}) ->
+syntax_parse0(#syntax_tree_parser{l_tree=L, r_tree=R, brackets_recursive=B, work_stack=['*'|T]}) ->
+    io:format("L:~w ~n", [L]),
+    io:format("R:~w ~n", [R]),
+    if 
+        L#syntax_node.value == '+' ->
+            Node = #syntax_node{l_child=L#syntax_node.r_child, r_child=R, type=operator, value='*'},
+            io:format("Node:~w ~n", [Node]),
+            syntax_parse0(#syntax_tree_parser{l_tree=L#syntax_node.l_child, r_tree=Node, brackets_recursive=B, work_stack=['+'|T]});
+        L#syntax_node.value == '-' ->
+            Node = #syntax_node{l_child=L#syntax_node.r_child, r_child=R, type=operator, value='*'},
+            syntax_parse0(#syntax_tree_parser{l_tree=L#syntax_node.l_child, r_tree=Node, brackets_recursive=B, work_stack=['-'|T]});
+        true ->
+            Node = #syntax_node{l_child=L, r_child=R, type=operator, value='*'},
+            syntax_parse0(#syntax_tree_parser{l_tree=Node, r_tree=null, brackets_recursive=B, work_stack=T})
+    end;
+
+syntax_parse0(#syntax_tree_parser{l_tree=L, r_tree=R, brackets_recursive=B, work_stack=['/'|T]}) ->
+    if 
+        L#syntax_node.value == '+' ->
+            Node = #syntax_node{l_child=L#syntax_node.r_child, r_child=R, type=operator, value='/'},
+            syntax_parse0(#syntax_tree_parser{l_tree=L#syntax_node.l_child, r_tree=Node, brackets_recursive=B, work_stack=['+'|T]});
+        L#syntax_node.value == '-' ->
+            Node = #syntax_node{l_child=L#syntax_node.r_child, r_child=R, type=operator, value='/'},
+            syntax_parse0(#syntax_tree_parser{l_tree=L#syntax_node.l_child, r_tree=Node, brackets_recursive=B, work_stack=['-'|T]});
+        true ->
+            Node = #syntax_node{l_child=L, r_child=R, type=operator, value='/'},
+            syntax_parse0(#syntax_tree_parser{l_tree=Node, r_tree=null, brackets_recursive=B, work_stack=T})
+    end;
+
+syntax_parse0(#syntax_tree_parser{l_tree=null, r_tree=null, brackets_recursive=B, work_stack=[H|T]}) ->
     Node = #syntax_node{type=operand, value=H},
-    syntax_parse(#syntax_tree_parser{l_tree=Node, r_tree=null, brackets_recursive=B, work_stack=T});
-syntax_parse(#syntax_tree_parser{l_tree=null, r_tree=R, brackets_recursive=B, work_stack=[H|T]}) ->
+    syntax_parse0(#syntax_tree_parser{l_tree=Node, r_tree=null, brackets_recursive=B, work_stack=T});
+syntax_parse0(#syntax_tree_parser{l_tree=null, r_tree=R, brackets_recursive=B, work_stack=[H|T]}) ->
     Node = #syntax_node{type=operand, value=H},
-    syntax_parse(#syntax_tree_parser{l_tree=Node, r_tree=R, brackets_recursive=B, work_stack=T});
-syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=null, brackets_recursive=B, work_stack=[H|T]}) ->
-    Node = #syntax_node{type=operand, value=H},
+    syntax_parse0(#syntax_tree_parser{l_tree=Node, r_tree=R, brackets_recursive=B, work_stack=T});
+syntax_parse0(#syntax_tree_parser{l_tree=L, r_tree=null, brackets_recursive=B, work_stack=[H|T]}) ->
     if
         L#syntax_node.type == operator ->
+            Node = #syntax_node{type=operand, value=H},
             Node1 = #syntax_node{l_child=L#syntax_node.l_child, r_child=Node, type=L#syntax_node.type, value=L#syntax_node.value},
-            syntax_parse(#syntax_tree_parser{l_tree=Node1, r_tree=null, brackets_recursive=B, work_stack=T});
+            syntax_parse0(#syntax_tree_parser{l_tree=Node1, r_tree=null, brackets_recursive=B, work_stack=T});
         true ->
-            null
-    end.
-    syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=Node, brackets_recursive=B, work_stack=T}).
+            syntax_error
+    end;
+
+syntax_parse0(_) ->
+    pattern_error.
+
+test() ->
+    syntax_parse0(#syntax_tree_parser{work_stack=[1,'+',2,'*',1]}).
