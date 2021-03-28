4b36c8966e46f18514786d68cea4de77e05da533
lambda <340035341@qq.com>
2021-03-28 15:52:48 +0800

    commit

--- rpn_calculator/rpn.erl
+++ rpn_calculator/rpn.erl
@@ -25,7 +25,7 @@ syntax_parse(#syntax_tree_parser{l_tree=L, work_stack=[]}) ->
     io:format("~w ~n", [L]),
     L;
 
-syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, work_stack=['('|T]}) ->
+syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, work_stack=["("|T]}) ->
     if
         L == null ->
             syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, work_stack=T});
@@ -34,56 +34,56 @@ syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, work_stack=['('|T]}) ->
             io:format("Node: ~w ~n", [Node]),
             syntax_parse(#syntax_tree_parser{l_tree=Node, r_tree=R, work_stack=T})
     end;
-syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, work_stack=[')'|T]}) ->
+syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, work_stack=[")"|T]}) ->
     Node = #syntax_node{l_child=L#syntax_node.l_child, r_child=L#syntax_node.r_child, type=L#syntax_node.type, value=L#syntax_node.value, pior=high},
     io:format("Node: ~w ~n", [Node]),
     syntax_parse(#syntax_tree_parser{l_tree=Node, r_tree=R, work_stack=T});
 
-syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, work_stack=['+'|T]}) ->
+syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, work_stack=["+"|T]}) ->
     if 
         L#syntax_node.pior == low ->
-            Node = #syntax_node{l_child=L#syntax_node.r_child, r_child=R, type=operator, value='+'},
+            Node = #syntax_node{l_child=L#syntax_node.r_child, r_child=R, type=operator, value="+"},
             Node1 = #syntax_node{l_child=L#syntax_node.l_child, r_child=Node, type=L#syntax_node.type, value=L#syntax_node.value, pior=L#syntax_node.pior},
             io:format("Node1: ~w ~n", [Node1]),
             syntax_parse(#syntax_tree_parser{l_tree=Node1, r_tree=null, work_stack=T});
         true ->
-            Node = #syntax_node{l_child=L, r_child=R, type=operator, value='+'},
+            Node = #syntax_node{l_child=L, r_child=R, type=operator, value="+"},
             syntax_parse(#syntax_tree_parser{l_tree=Node, r_tree=null, work_stack=T})
     end;
-syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, work_stack=['-'|T]}) ->
+syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, work_stack=["-"|T]}) ->
     if 
         L#syntax_node.pior == low ->
-            Node = #syntax_node{l_child=L#syntax_node.r_child, r_child=R, type=operator, value='-'},
+            Node = #syntax_node{l_child=L#syntax_node.r_child, r_child=R, type=operator, value="-"},
             Node1 = #syntax_node{l_child=L#syntax_node.l_child, r_child=Node, type=L#syntax_node.type, value=L#syntax_node.value, pior=L#syntax_node.pior},
             io:format("Node1: ~w ~n", [Node1]),
             syntax_parse(#syntax_tree_parser{l_tree=Node1, r_tree=null, work_stack=T});
         true ->
-            Node = #syntax_node{l_child=L, r_child=R, type=operator, value='-'},
+            Node = #syntax_node{l_child=L, r_child=R, type=operator, value="-"},
             syntax_parse(#syntax_tree_parser{l_tree=Node, r_tree=null, work_stack=T})
     end;
 
-syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, work_stack=['*'|T]}) ->
+syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, work_stack=["*"|T]}) ->
     if 
-        L#syntax_node.value == '+', L#syntax_node.pior /= high ->
-            Node = #syntax_node{l_child=L#syntax_node.r_child, r_child=R, type=operator, value='*'},
-            syntax_parse(#syntax_tree_parser{l_tree=L#syntax_node.l_child, r_tree=Node, work_stack=['+'|T]});
-        L#syntax_node.value == '-', L#syntax_node.pior /= high ->
-            Node = #syntax_node{l_child=L#syntax_node.r_child, r_child=R, type=operator, value='*'},
-            syntax_parse(#syntax_tree_parser{l_tree=L#syntax_node.l_child, r_tree=Node, work_stack=['-'|T]});
+        L#syntax_node.value == "+", L#syntax_node.pior /= high ->
+            Node = #syntax_node{l_child=L#syntax_node.r_child, r_child=R, type=operator, value="*"},
+            syntax_parse(#syntax_tree_parser{l_tree=L#syntax_node.l_child, r_tree=Node, work_stack=["+"|T]});
+        L#syntax_node.value == "-", L#syntax_node.pior /= high ->
+            Node = #syntax_node{l_child=L#syntax_node.r_child, r_child=R, type=operator, value="*"},
+            syntax_parse(#syntax_tree_parser{l_tree=L#syntax_node.l_child, r_tree=Node, work_stack=["-"|T]});
         true ->
-            Node = #syntax_node{l_child=L, r_child=R, type=operator, value='*'},
+            Node = #syntax_node{l_child=L, r_child=R, type=operator, value="*"},
             syntax_parse(#syntax_tree_parser{l_tree=Node, r_tree=null, work_stack=T})
     end;
-syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, work_stack=['/'|T]}) ->
+syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=R, work_stack=["/"|T]}) ->
     if 
-        L#syntax_node.value == '+', L#syntax_node.pior /= high ->
-            Node = #syntax_node{l_child=L#syntax_node.r_child, r_child=R, type=operator, value='/'},
-            syntax_parse(#syntax_tree_parser{l_tree=L#syntax_node.l_child, r_tree=Node, work_stack=['+'|T]});
-        L#syntax_node.value == '-', L#syntax_node.pior /= high ->
-            Node = #syntax_node{l_child=L#syntax_node.r_child, r_child=R, type=operator, value='/'},
-            syntax_parse(#syntax_tree_parser{l_tree=L#syntax_node.l_child, r_tree=Node, work_stack=['-'|T]});
+        L#syntax_node.value == "+", L#syntax_node.pior /= high ->
+            Node = #syntax_node{l_child=L#syntax_node.r_child, r_child=R, type=operator, value="/"},
+            syntax_parse(#syntax_tree_parser{l_tree=L#syntax_node.l_child, r_tree=Node, work_stack=["+"|T]});
+        L#syntax_node.value == "-", L#syntax_node.pior /= high ->
+            Node = #syntax_node{l_child=L#syntax_node.r_child, r_child=R, type=operator, value="/"},
+            syntax_parse(#syntax_tree_parser{l_tree=L#syntax_node.l_child, r_tree=Node, work_stack=["-"|T]});
         true ->
-            Node = #syntax_node{l_child=L, r_child=R, type=operator, value='/'},
+            Node = #syntax_node{l_child=L, r_child=R, type=operator, value="/"},
             syntax_parse(#syntax_tree_parser{l_tree=Node, r_tree=null, work_stack=T})
     end;
 
@@ -106,6 +106,9 @@ syntax_parse(#syntax_tree_parser{l_tree=L, r_tree=null, work_stack=[H|T]}) ->
 syntax_parse(_) ->
     pattern_error.
 
+parse(L) ->
+    syntax_parse(#syntax_tree_parser{work_stack=L}).
+
 concat([H|T], L2, Res) ->
     concat(T, L2, [H|Res]);
 concat([], [H|T], Res) ->
@@ -123,29 +126,62 @@ postorder_trav(_) ->
 to_list(Tree) ->
     postorder_trav(Tree).
 
-rpn(['+'|T], [Op1,Op2|Stack]) ->
+string_to_number(S) ->
+    case string:to_float(S) of 
+        {error, no_float} -> list_to_integer(S);
+        {F, _} -> F
+    end.
+
+rpn(["+"|T], [Op1,Op2|Stack]) ->
     rpn(T, [Op2+Op1|Stack]);
-rpn(['-'|T], [Op1,Op2|Stack]) ->
+rpn(["-"|T], [Op1,Op2|Stack]) ->
     rpn(T, [Op2-Op1|Stack]);
-rpn(['*'|T], [Op1,Op2|Stack]) ->
+rpn(["*"|T], [Op1,Op2|Stack]) ->
     rpn(T, [Op2*Op1|Stack]);
-rpn(['/'|T], [Op1,Op2|Stack]) ->
+rpn(["/"|T], [Op1,Op2|Stack]) ->
     rpn(T, [Op2/Op1|Stack]);
 rpn([H|T], Stack) ->
-    rpn(T, [H|Stack]);
+    rpn(T, [string_to_number(H)|Stack]);
 rpn([], [H|_]) ->
     H.
 rpn(L) ->
     rpn(L, []).
 
+tokens([H|T], _) ->
+    H == "(";
+tokens(["("|T], [C|S]) ->
+    tokens(T, ["(", C|S]);
+tokens([")"|T], [C|S]) ->
+    tokens(T, [")", C|S]);
+tokens(["+"|T], [C|S]) ->
+    tokens(T, ["+", C|S]);
+tokens(["-"|T], [C|S]) ->
+    tokens(T, ["-", C|S]);
+tokens(["*"|T], [C|S]) ->
+    tokens(T, ["*", C|S]);
+tokens(["/"|T], [C|S]) ->
+    tokens(T, ["/", C|S]);
+tokens([H|T], [C|S]) ->
+    tokens(T, [[H,C]|S]);
+tokens([], S) ->
+    lists:reverse(S).
+tokens(S) ->
+    tokens(S, []).
+
+eval(L) ->
+    rpn(to_list(parse(string:tokens(L, " ")))).
+
 test() ->
-    % T = syntax_parse(#syntax_tree_parser{work_stack=[10,'*',1,'+',2,'*',3,'-',4,'*',5,'/',6]}),
-    % syntax_parse(#syntax_tree_parser{work_stack=[10,'*',1]}).
-    % syntax_parse(#syntax_tree_parser{work_stack=['(','(',1,'-',2,')','+',3,')','*',4]}).
-    % syntax_parse(#syntax_tree_parser{work_stack=['(',1,'-',2,'+',3,')','*',4]}).
-    % syntax_parse(#syntax_tree_parser{work_stack=['(',1,'-',2,'*',3,')','*',4]}).
-    % syntax_parse(#syntax_tree_parser{work_stack=[1,'*','(',2,'+',3,')']}).
-    T=syntax_parse(#syntax_tree_parser{work_stack=['(',1,'-',2,')','*','(',3,'+',4,')']}),
-    L=to_list(T),
-    rpn(L).
+    % T = syntax_parse(#syntax_tree_parser{work_stack=[10,"*",1,"+",2,"*",3,"-",4,"*",5,"/",6]}),
+    % syntax_parse(#syntax_tree_parser{work_stack=[10,"*",1]}).
+    % syntax_parse(#syntax_tree_parser{work_stack=["(","(",1,"-",2,")","+",3,")","*",4]}).
+    % syntax_parse(#syntax_tree_parser{work_stack=["(",1,"-",2,"+",3,")","*",4]}).
+    % syntax_parse(#syntax_tree_parser{work_stack=["(",1,"-",2,"*",3,")","*",4]}).
+    % syntax_parse(#syntax_tree_parser{work_stack=[1,"*","(",2,"+",3,")"]}).
+    % T=syntax_parse(#syntax_tree_parser{work_stack=["(",1,"-",2,")","*","(",3,"+",4,")"]}),
+    % L=to_list(T),
+    % rpn(L).
+    % string:tokens("( 1 - 2 ) * ( 3 + 4 )", " ").
+    % eval("( 1.0 + 2 ) * ( 3 + 4 )").
+    tokens("(1+2)*(3+4)").
     
